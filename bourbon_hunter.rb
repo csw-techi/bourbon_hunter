e039a1a34405d25c3a2dd8b9cd91920f32d97058
Stephen Schor <stephen.schor@gmail.com>
2023-01-13 11:36:58 -0500

    Update the CSS selector to only grab available products

--- bourbon_hunter.rb
+++ bourbon_hunter.rb
@@ -2,13 +2,16 @@ require 'open-uri'
 require 'nokogiri'
 require 'csv'
 
+# We only care about available products
+AVAILABLE_PRODUCT_CSS_SELECTOR = '.container.section.container--justify-center.container--product-availability-available.aem-GridColumn--offset--tablet--0.aem-GridColumn--default--none.aem-GridColumn--phone--none.aem-GridColumn--phone--12.aem-GridColumn--tablet--none.aem-GridColumn.aem-GridColumn--tablet--6.aem-GridColumn--offset--phone--0.aem-GridColumn--offset--default--0.aem-GridColumn--default--4'
+
   def scraping(url)
     html = URI.open("#{url}").read
     nokogiri_doc = Nokogiri::HTML(html)
     final_array = []
 
-    nokogiri_doc.search(".cmp-title h3").each do |element|
-      element = element.text
+    nokogiri_doc.search(AVAILABLE_PRODUCT_CSS_SELECTOR).each do |element|
+      element = element.search('.cmp-title h3').text
       final_array << element
     end
       final_array.each_with_index do |element, index|
